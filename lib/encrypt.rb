d7d428e9839f933953b1967e8898a529dea1a659
joshuajhun <joshuajhun@gmail.com>
2015-09-15 18:02:14 -0600

    tuesday

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,35 +1,88 @@
 class Encrypt
-  def initialize
-    @key = rand(10000..99999)
+  attr_reader :input,
+              :date
+
+  def initialize(key, input = "Thanks")
+    @key = key
+    @date = Time.now.strftime("%d%m%y").to_i
+    @input = input
   end
 
   def key_generator
-    a = @key.to_s[0..1]
-    b = @key.to_s[1..2]
-    c = @key.to_s[2..3]
-    d = @key.to_s[3..4]
-    array = [a, b, c, d]
+    @key = 5.times.map { ('0'..'9').to_a.sample }.join
   end
 
-  def offset
-    date = Time.now.strftime("%d%m%y").to_i
-    square_of_date = date * date
-    a_offset = square_of_date.to_s[-4]
-    b_offset = square_of_date.to_s[-3]
-    c_offset = square_of_date.to_s[-2]
-    d_offset = square_of_date.to_s[-1]
+  def key_a_rotation
+    a = @key.to_s[0..1].to_i
+  end
+
+  def key_b_rotation
+    b = @key.to_s[1..2].to_i
+  end
+
+  def key_c_rotation
+    c = @key.to_s[2..3].to_i
+  end
+
+  def key_d_rotation
+    d = @key.to_s[3..4].to_i
+  end
+
+  def square_the_date
+    square_of_date = @date * @date
+  end
+
+  def a_offset
+    a_offset = square_the_date.to_s[-4].to_i
+  end
+
+  def b_offset
+    b_offset = square_the_date.to_s[-3].to_i
+  end
+
+  def c_offset
+    c_offset = square_the_date.to_s[-2].to_i
+  end
+
+  def d_offset
+    d_offset = square_the_date.to_s[-1].to_i
   end
 
   def character_map
     character_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
-      "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u"
-      "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6"
+      "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+      "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6",
       "7", "8", "9", " ", ".", ","]
   end
-end
 
-Encrypt.new.key_generator
-Encrypt.new.offset
+  def downcase_string
+    downcase_string = input.downcase
+  end
+
+  def chars_string
+    chars_array = downcase_string.chars
+  end
+
+  def iteration
+    chars_string.map!.with_index do |char, index|
+      if index == 0
+        char = "q"
+      else
+        char
+      end
+    end
+  end
+
+  # iterate over elements based on index modulo 4 == 0/1/2/3
+  # rotate by key of a rotation plus a offset or ...
+  # where rotate involves first pulling the index of the
+  # element in the character map and adding the rotation offset
+  # and rotating the character map by that full amount then
+  # using shift on the character map array to return encrypted
+  # character which we then map onto the input string array
+  # array.join at again
+
+end
 
 if __FILE__==$0
   def date(date)
@@ -37,7 +90,7 @@ if __FILE__==$0
   end
 
   input_string = File.open(ARGV[0]).read
-  key = rand(10000..99999)
+  key = 5.times.map { ('0'..'9').to_a.sample }.join
   output_file = File.open(ARGV[1], "w")
   output_string = "Hello"
   output_file.write(output_string)
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,8 +1,68 @@
 gem 'minitest', '~> 5.0'
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../encrypt.rb'
+require '../lib/encrypt.rb'
 
-class EncryptTest
-  
+class EncryptTest < Minitest::Test
+
+  def test_that_we_can_test_randomness
+    size = 100.times.map { Encrypt.new(12345).key_generator }.uniq.size
+    assert_operator 1, :<, size
+  end
+
+  def test_that_it_can_produce_a_date_integer_with_todays_date
+    encrypt = Encrypt.new(12345)
+    assert_equal 150915, encrypt.date
+  end
+
+  def test_key_rotations_are_created
+    encrypt = Encrypt.new(12345)
+    assert_equal 12, encrypt.key_a_rotation
+    assert_equal 23, encrypt.key_b_rotation
+    assert_equal 34, encrypt.key_c_rotation
+    assert_equal 45, encrypt.key_d_rotation
+  end
+
+  def test_date_is_todays_date
+    encrypt = Encrypt.new(12345)
+    assert_equal 150915, encrypt.date
+  end
+
+  def test_that_square_the_date_method_works
+    encrypt = Encrypt.new(12345)
+    assert_equal 22775337225, encrypt.square_the_date
+  end
+
+  def test_offsets_are_created
+    encrypt = Encrypt.new(12345)
+    assert_equal 7, encrypt.a_offset
+    assert_equal 2, encrypt.b_offset
+    assert_equal 2, encrypt.c_offset
+    assert_equal 5, encrypt.d_offset
+  end
+
+  def test_we_can_downcase_an_input_string
+    encrypt = Encrypt.new(12345)
+    assert_equal "thanks", encrypt.downcase_string
+  end
+
+  def test_we_can_chars_an_input_string
+    encrypt = Encrypt.new(12345)
+    assert_equal ["t", "h", "a", "n", "k", "s"], encrypt.chars_string
+  end
+
+  # pass in non defaults
+
+  def test_we_can_iterate_over_first_element
+    encrypt = Encrypt.new(12345)
+    assert_equal "q", encrypt.iteration[0]
+  end
+
+  # def test_it_can_rotate_in_the_array
+  #   skip
+  #   encrypt = Encrypt.new(12345)
+  #   encrypt.character_map.rotate(5)
+  #   assert_equal "e", encrypt.character_map.rotate(5)
+  # end
+
 end
