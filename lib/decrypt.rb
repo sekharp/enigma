7d0326fccecf28087db8203869e88f64c5db7fe4
joshuajhun <joshuajhun@gmail.com>
2015-09-16 16:20:29 -0600

    decrypt done, file io works, missing test though

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,15 +1,16 @@
-class Encrypt
+class Decrypt
   attr_reader :input,
-              :date
+              :date,
+              :key
 
-  def initialize(key = 12345, input = "Thanks")
+  def initialize(key = 12345, input = ",6 y3e")
     @key = key
     @date = Time.now.strftime("%d%m%y").to_i
     @input = input
   end
 
-  def key_generator
-    @key = 5.times.map { ('0'..'9').to_a.sample }.join
+  def chars_string
+    input.chars
   end
 
   def key_rotation(wheel)
@@ -17,15 +18,15 @@ class Encrypt
   end
 
   def square_the_date
-    @date * @date
+    square_of_date = @date * @date
   end
 
   def offset(wheel)
-    square_the_date.to_s[wheel-5].to_i
+    offset = square_the_date.to_s[wheel-5].to_i
   end
 
   def full_rotation(wheel)
-    key_rotation(wheel) + offset(wheel)
+    full_rotation = key_rotation(wheel) + offset(wheel)
   end
 
   def character_map
@@ -35,30 +36,22 @@ class Encrypt
       "7", "8", "9", " ", ".", ","]
   end
 
-  def downcase_string
-    input.downcase
-  end
-
-  def chars_string
-    downcase_string.chars
-  end
-
-  def encryption
+  def decryption
     chars_string.map!.with_index do |char, index|
       if index % 4 == 0
-        rotation = character_map.index("#{char}") + full_rotation(1)
+        rotation = character_map.index("#{char}") - full_rotation(1)
         new_char = character_map.rotate(rotation).shift
         char = new_char
       elsif index % 4 == 1
-        rotation = character_map.index("#{char}") + full_rotation(2)
+        rotation = character_map.index("#{char}") - full_rotation(2)
         new_char = character_map.rotate(rotation).shift
         char = new_char
       elsif index % 4 == 2
-        rotation = character_map.index("#{char}") + full_rotation(3)
+        rotation = character_map.index("#{char}") - full_rotation(3)
         new_char = character_map.rotate(rotation).shift
         char = new_char
       else index % 4 == 3
-        rotation = character_map.index("#{char}") + full_rotation(4)
+        rotation = character_map.index("#{char}") - full_rotation(4)
         new_char = character_map.rotate(rotation).shift
         char = new_char
       end
@@ -66,17 +59,17 @@ class Encrypt
   end
 
   def join_chars_string
-    encryption.join
+    decryption.join
   end
 
 end
 
 if __FILE__==$0
-  input_string = File.open(ARGV[0]).read
-  key = 5.times.map { ('0'..'9').to_a.sample }.join
+  input_string = File.read(ARGV[0])
+  key = ARGV[2]
   output_file = File.open(ARGV[1], "w")
-  output_string = Encrypt.new(key, "#{input_string}").join_chars_string
+  output_string = Decrypt.new(key, "#{input_string}").join_chars_string
   output_file.write(output_string)
   date = Time.now.strftime("%d%m%y")
-  puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
+  puts "Created '#{ARGV[1]}' with the key #{ARGV[2]} and date #{ARGV[3]}"
 end
