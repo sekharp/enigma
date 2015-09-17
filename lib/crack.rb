c5278aa6a54e03f37091e9db0ce6088ab2d0cd7c
Sekhar Paladugu <sekharp@gmail.com>
2015-09-17 11:10:53 -0600

    packaged

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,46 +1,46 @@
 class Cracked
-  attr_reader :input,  # => :input
-              :date,   # => :date
-              :key     # => nil
+  attr_reader :input,
+              :date,
+              :key
 
   def initialize(input = ",6 y3e")
-    @key = key                                # => nil
-    @date = Time.now.strftime("%d%m%y").to_i  # => 160915
-    @input = input                            # => ",6 y3e"
-  end                                         # => :initialize
+    @key = key
+    @date = Time.now.strftime("%d%m%y").to_i
+    @input = input
+  end
 
   def crack_key
     correction = @input.length % 4
     encrypted_char = @input[-4-correction] # char is a rotation from .
     full_rotation(1) = character_map.index("#{encrypted_char}") - rotation
-  end                               # => :crack_key
+  end
 
   def chars_string
     input.chars
-  end               # => :chars_string
+  end
 
   def key_rotation(wheel)
     @key.to_s[wheel-1..wheel].to_i
-  end                               # => :key_rotation
+  end
 
   def square_the_date
     square_of_date = @date * @date
-  end                               # => :square_the_date
+  end
 
   def offset(wheel)
     offset = square_the_date.to_s[wheel-5].to_i
-  end                                            # => :offset
+  end
 
   def full_rotation(wheel)
     full_rotation = key_rotation(wheel) + offset(wheel)
-  end                                                    # => :full_rotation
+  end
 
   def character_map
     character_map = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
       "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
       "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6",
       "7", "8", "9", " ", ".", ","]
-  end                                                              # => :character_map
+  end
 
   def decryption
     chars_string.map!.with_index do |char, index|
@@ -62,18 +62,18 @@ class Cracked
         char = new_char
       end
     end
-  end                                                                 # => :decryption
+  end
 
   def join_chars_string
     decryption.join
-  end                    # => :join_chars_string
+  end
 
-end  # => :join_chars_string
+end
 
-Cracked.new(",6 y3e").input[-6]  # => ","
+Cracked.new(",6 y3e").input[-6]
 
-if __FILE__==$0                                                                # => true
-  input_string = File.read(ARGV[0])                                            # ~> TypeError: no implicit conversion of nil into String
+if __FILE__==$0
+  input_string = File.read(ARGV[0])
   key = key
   output_file = File.open(ARGV[1], "w")
   output_string = Cracked.new(key, "#{input_string}").join_chars_string
@@ -81,9 +81,3 @@ if __FILE__==$0                                                                #
   date = Time.now.strftime("%d%m%y")
   puts "Created '#{ARGV[1]}' with the cracked key #{key} and date #{ARGV[2]}"
 end
-
-# ~> TypeError
-# ~> no implicit conversion of nil into String
-# ~>
-# ~> /Users/sekharpaladugu/code/projects/enigma/lib/crack.rb:75:in `read'
-# ~> /Users/sekharpaladugu/code/projects/enigma/lib/crack.rb:75:in `<main>'
